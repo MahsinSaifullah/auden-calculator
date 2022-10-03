43a5844215339d649d9e2ee5c25976cceade6613
macbookpro <mahsinsaifullah@gmail.com>
2022-10-03 11:51:59 +0100

    chore: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import styled from 'styled-components';
 
-function App() {
+import { Calculator } from './components';
+
+const MainWrapper = styled.main`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainWrapper>
+      <Calculator />
+    </MainWrapper>
   );
-}
+};
 
 export default App;
---
+++ src/components/Calculator.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const Calculator: React.FC = () => {
+  return <div>Calculator</div>;
+};
